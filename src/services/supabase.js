5517cb847fb38a1f5273024d01fd8f230b606840
Devansh1741 <rathoredevansh1741@gmail.com>
2024-11-06 21:41:42 +0530

    Layout setup

---
+++ src/services/supabase.js
@@ -0,0 +1,8 @@
+import { createClient } from "@supabase/supabase-js";
+
+const supabaseUrl = "https://hqjjoqtokhitebowisri.supabase.co";
+const supabaseKey =
+  "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImhxampvcXRva2hpdGVib3dpc3JpIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MzA4OTMyNjAsImV4cCI6MjA0NjQ2OTI2MH0.CIpzspvPURCp-pgI5JYNrkDR4I_QGrUbfiNp5Gt-DTw";
+const supabase = createClient(supabaseUrl, supabaseKey);
+
+export default supabase;
