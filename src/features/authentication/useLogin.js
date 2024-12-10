e7a0b203c2018fa9bd7fc04e9e63012f1d038f17
Devansh1741 <rathoredevansh1741@gmail.com>
2024-12-10 14:12:24 +0530

    Authentication

---
+++ src/features/authentication/useLogin.js
@@ -0,0 +1,26 @@
+import { useMutation, useQueryClient } from "@tanstack/react-query";
+import { login as LoginApi } from "../../services/apiAuth";
+import { useNavigate } from "react-router-dom";
+import toast from "react-hot-toast";
+
+export function useLogin() {
+  const queryClient = useQueryClient();
+
+  const navigate = useNavigate();
+
+  const { mutate: login, isLoading } = useMutation({
+    mutationFn: ({ email, password }) => LoginApi({ email, password }),
+    onSuccess: (data) => {
+      queryClient.invalidateQueries(["user"]);
+      queryClient.setQueryData(["user", data.user]);
+      navigate("/dashboard", { replace: true });
+    },
+
+    onError: (err) => {
+      console.error("Error", err);
+      toast.error("Provided email or password are incorrect");
+    },
+  });
+
+  return { login, isLoading };
+}
