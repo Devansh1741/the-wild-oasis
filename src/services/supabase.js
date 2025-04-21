9ae6e6d8ff4e03f46b56a0845db182d3d3084db4
Devansh1741 <rathoredevansh1741@gmail.com>
2025-04-21 23:09:19 +0530

    .env fix

--- src/services/supabase.js
+++ src/services/supabase.js
@@ -1,7 +1,7 @@
 import { createClient } from "@supabase/supabase-js";
 
 export const supabaseUrl = "https://hqjjoqtokhitebowisri.supabase.co";
-const supabaseKey = {process.env.SUPABASE_SECRET_KEY};
+const supabaseKey = import.meta.env.SUPABASE_SECRET_KEY;
 const supabase = createClient(supabaseUrl, supabaseKey);
 
 export default supabase;
